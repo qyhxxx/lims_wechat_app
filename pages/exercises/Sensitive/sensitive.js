a977c677110ae2b71997af5b0f0b102df38a1c89
zhangkai <zhangshangkai@qq.com>
2018-09-10 15:57:36 +0800

    进一步完善了饮用水嗅味喜好调查的上下一题逻辑，并且与api接口成功进行数据交互，实现了页面之间的传参

--- pages/exercises/Sensitive/sensitive.js
+++ pages/exercises/Sensitive/sensitive.js
@@ -1,14 +1,19 @@
 // pages/exercises/Sensitive/sensitive.js
+var app = getApp()
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    text: '饮用水喜好调查',
-    text1: '物质浓度-嗅味强度效应曲线',
-    text2: 'UPSIT嗅味测试簿调查',
-    textGuide: '方法介绍：该方法主要通过测试者提供的喜好得分来评估消费者对饮用水感官特征的态度。',
+   // text: '饮用水喜好调查',
+   //text1: '物质浓度-嗅味强度效应曲线',
+    //text2: 'UPSIT嗅味测试簿调查',
+   // textGuide: '方法介绍：该方法主要通过测试者提供的喜好得分来评估消费者对饮用水感官特征的态度。',
+    trainItemId: [0 , 0 , 0],
+    title: ["饮用水嗅味喜好调查", "物质浓度-嗅味强度效应曲线", "UPSIT嗅味测试簿调查"],
+    textGuide: ["", "", ""],
+    questionType: ["", "", ""],
     modalHidden: true //是否隐藏对话框
   },
 
@@ -22,6 +27,14 @@ Page({
   },
   // 开始答题 按钮点击事件
   modalBindaconfirm: function () {
+    var trainItemId = this.data.trainItemId[0]
+    var questionType = this.data.questionType[0]
+    console.log(trainItemId)
+    wx.setStorageSync('trainItemId', trainItemId)
+    // wx.setStorage({
+    //   key: 'questionType',
+    //   data: questionType,
+    // })
     wx.navigateTo({
       url: '/pages/survey/drinking_water/drinking_water',
     })
@@ -50,7 +63,30 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    var postData = {
+      "trainingType": "嗅味敏感性调查"
+    }
+    var that = this
+    app.functions.authRequest('/app/smell/training/item/list', 'POST', postData, function (res) {
+      console.log(res)
+      var trainItemId = that.data.trainItemId
+      var title = that.data.title
+      var description = that.data.textGuide
+      var questionType = that.data.questionType
+      for (var i = 0; i < 3; i++) {
+        trainItemId[i] = res["data"][i]["id"]
+        title[i] = res["data"][i]["title"]
+        description[i] = res["data"][i]["description"]
+        questionType[i] = res["data"][i]["questionType"]
+      }
+      that.setData({
+        trainItemId: trainItemId,
+        title: title,
+        textGuide: description,
+        questionType: questionType
+      })
+      console.log(that.data.title)
+    })
   },
 
   /**
@@ -64,7 +100,7 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+    
   },
 
   /**
