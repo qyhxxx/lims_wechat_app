c4ed602ff84417e1c8362486c191a215eafa9cd0
步月随影 <1368205282@qq.com>
2018-09-11 13:14:46 +0800

    饮用水喜好界面再完善

--- pages/exercises/UPSIT/UPSIT.js
+++ pages/exercises/UPSIT/UPSIT.js
@@ -5,6 +5,30 @@ Page({
    * 页面的初始数据
    */
   data: {
+    option: [
+      { value: '0', name: 'A   ' },
+      { value: '1', name: 'B   ' },
+      { value: '2', name: 'C   ' },
+      { value: '3', name: 'D   ' }
+    ],
+    title: ['题目1：这个气味闻起来最像 ', '题目2：这个气味闻起来最像 '],
+    button_pre: '上一题',
+    button_next: '下一题',
+    exercise_num: 1,
+    radio: 'option-radio-unchecked',
+    radio_value: -1,
+    post_value: [-1, -1],
+    exercise_id: [-1, -1], //题目id，再页面onLoad时从服务器端获取数据
+    final_answer: [
+      {
+        id: -1,
+        num: -1
+      },
+      {
+        id: -1,
+        num: -1
+      }
+    ]
 
   },
 
